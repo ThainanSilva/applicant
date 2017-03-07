7b2aa233c1f3f77114358e0bc84e37fd30a24b8b
5fcloud <>
2017-03-07 16:43:17 -0400

    Correção de arquivo

--- api/addNewUser.php
+++ api/addNewUser.php
@@ -4,7 +4,7 @@ require_once('classes/db.php');
 
 
 if (isset($_POST['email']) and isset($_POST['profile']) and $_POST['email'] != "" and $_POST['profile'] != ""){
-    if()
+ 
     
     echo $user->jsonfyResponse('success', 'welcome');
 }else{
